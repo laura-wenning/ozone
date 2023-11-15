d5d0c396cd65fccabcec6c8787f2c070bdecf5c4
Laura Wenning <laura.r.wenning@gmail.com>
2023-11-15 13:02:53 -0500

    Changes from linting and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use self::discord_bot::discord_start;
-use dotenv;
 
 mod discord_bot;
 
 #[tokio::main]
 async fn main() {
-	dotenv::dotenv().ok();
+    dotenv::dotenv().ok();
 
-	println!("Hello, world!");
-	let discord_thread = discord_start();
-	println!("Yo!");
+    println!("Hello, world!");
+    let discord_thread = discord_start();
+    println!("Yo!");
 
-	discord_thread.await;
+    discord_thread.await;
 }
